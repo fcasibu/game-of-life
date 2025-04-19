20e4be9301466e0c5f8bb7733b50dd99722d6040
Francis Eugene Casibu <projectnevz@gmail.com>
2025-04-19 14:12:10 +0800

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+    ],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-non-null-assertion': 'off',
+    },
+  },
+);
---
+++ src/app.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import Counter from './lib/components/Counter.svelte';
+</script>
+
+<main>
+  <div>
+    <div>
+      <Counter defaultCount={10} />
+    </div>
+  </div>
+</main>
